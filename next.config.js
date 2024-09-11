7922f758bed29b7983f6af6123f1e7b605636494
Maaz Gheewale <maazghe@gmail.com>
2024-09-11 21:03:29 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  output: 'export',
-  assetPrefix: 'https://mmgheewale.github.io/portfolio/',
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  distDir: 'dist',
-}
- 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+
+const env = process.env.NODE_ENV;
+
+const nextConfig =
+  env === "development"
+    ? {}
+    : {
+        output: "export",
+        distDir: 'dist',
+        assetPrefix: "https://mmgheewale.github.io/portfolio/",
+      };
+
+module.exports = nextConfig;
