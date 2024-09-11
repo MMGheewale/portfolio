31bb117368730cd4fbf08a7a916d409260534c10
MMGheewale <mohammedmaazgheewale@gmail.com>
2024-09-11 17:48:08 +0200

    changed

--- next.config.js
+++ next.config.js
@@ -11,18 +11,9 @@ const nextConfig = {
   // skipTrailingSlashRedirect: true,
  
   // Optional: Change the output directory `out` -> `dist`
-  distDir: 'dist',
+  // distDir: 'dist',
+}
  
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/en.html',
-      },
-    ];
-  },
-};
-
 module.exports = nextConfig
 
 /*
